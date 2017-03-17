5e79f2cbfa6a32bc7ecc2b151de3f2a65cac09e2
freelyhx <787126844@qq.com>
2017-03-17 20:06:42 +0800

    add camInfo.sh

---
+++ cameraInfomation/camInfo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#-----------------------------------------------------------
+# filename:camInfo.sh
+# version:1.0
+# date:2017/03/17
+# author:yangql
+# program:Camera running status information
+#-----------------------------------------------------------
+
+top -n 1 > /tmp/top.log
+TEMP=$(cat /sys/bus/i2c/devices/0-004f/temp1_input)
+VSZ=$(egrep Traffic /tmp/top.log | awk '{printf $5}' | egrep -o [0-9]+m)
+CPU=$(egrep Traffic /tmp/top.log | awk '{printf $7}')
+USED=$(egrep Mem /tmp/top.log | awk '{printf $2}')
+FREE=$(egrep Mem /tmp/top.log | awk '{printf $4}')
+CACHED=$(egrep Mem /tmp/top.log | awk '{printf $10}')
+
+printf "%-10s %-10s %-10s %-10s %-10s %-10s\n" VSZ CPU USED FREE CACHED TEMP
+printf "%-10s %-10s %-10s %-10s %-10s %-10i\n" $VSZ $CPU $USED $FREE $CACHED $TEMP
