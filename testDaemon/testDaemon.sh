7dbee84e23bba632efbbc21d907ff996b3065976
freelyhx <787126844@qq.com>
2017-03-22 09:19:24 +0800

    优化判断逻辑，增加对无进程时的处理

--- testDaemon/testDaemon.sh
+++ testDaemon/testDaemon.sh
@@ -2,50 +2,61 @@
 
 #-----------------------------------------------------------
 # filename:testDaemon.sh
-# version:1.0
-# date:2017/03/20
+# version:2.0
+# date:2017/03/21
 # author:yangql
 # program:测试守护程序，是否可以成功守护NvrServer、OperateServer、IVE_update、appweb这4个进程
 #-----------------------------------------------------------
 
-#PNAME=NvrServer
 
+COUNT=0		#计数器,从0开始计数
+TIME=10		#每次等待时间，默认10秒
+MAX=5		#等待次数，默认5次
 
 daemonProcess(){
+	top -n 1 | grep $* > /dev/null
+	s=$?
+	if [ $s != 0 ]
+	then
+		echo "error!!"
+		echo "$* process does not exit!"
+		return 1
+	fi
 
 	oldPID=$(top -n 1 | grep $* | awk '{print $1}')
+	echo "$* oldPID is:$oldPID"
 	pkill $*
 	
-	while 1
+	while true
 	do
-		top -n 1 | grep $*
+		if [ $COUNT -ge $MAX ]
+		then
+			echo "time out! daemon $* failed."
+			COUNT=0
+			return 1
+		fi
+	
+		top -n 1 | grep $* > /dev/null
 		status=$?
-		if [ status == 0 ]
+		if [ $status == 0 ]
 		then
 			newPID=$(top -n 1 | grep $* | awk '{print $1}')
-			break
+			if [ $oldPID != $newPID ]
+			then
+				echo "$* newPID is:$newPID"
+				echo "daemon $* sucess!"
+				COUNT=0
+			fi
+			return 0
 		fi
+		
+		let COUNT++
+		echo "$COUNT wait $TIME second."
+		sleep $TIME
 	done
-	
-	# sleep 50
-	
-	# newPID=$(top -n 1 | grep $* | awk '{print $1}')
-	
-	if [ $oldPID != $newPID ]
-	then
-		echo "$* oldPID is:$oldPID, newPID is:$newPID"
-		echo "daemon $* sucess!"
-	elif [ $oldPID == $newPID ]
-	then
-		echo "$* oldPID is:$oldPID, newPID is:$newPID"
-		echo "daemon $* failed!"
-	else
-		echo "daemon $* failed! "
-	fi
 }
 
 daemonProcess NvrServer
-# daemonProcess OperateServer
-# daemonProcess IVE_update
-# daemonProcess appweb
-
+daemonProcess OperateServer
+daemonProcess IVE_update
+daemonProcess appweb
