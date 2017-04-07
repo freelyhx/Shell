b5ec6319a17ab67bd522107baa26f4b85459fe8f
freelyhx <787126844@qq.com>
2017-04-07 17:49:37 +0800

    优化脚本，精炼执行语句

--- update_lib/up_lib.sh
+++ update_lib/up_lib.sh
@@ -16,15 +16,16 @@ targetDir=/opt/work/s2/
 #查看lib个数并写入数组
 cd $sourceDir
 Lib=($(ls *.so))
+libLength=${#Lib[*]}
 echo "要替换${#Lib[@]}个文件，文件分别为："
-for (( i = 0; i < ${#Lib[@]}; ++i ))
+for (( i = 0; i < libLength; ++i ))
 do
   echo "${Lib[i]}"
 done
 
 
 #备份被替换lib文件
-for (( i = 0; i < ${#Lib[@]}; ++i ))
+for (( i = 0; i < $libLength; ++i ))
 do
   mv $targetDir/${Lib[i]} $targetDir/${Lib[i]}-back
   echo "."
@@ -35,17 +36,20 @@ ls -l $targetDir/*.so-back
 
 
 #替换lib文件到目标目录
-for (( i = 0; i < ${#Lib[@]}; ++i ))
+for (( i = 0; i < $libLength; ++i ))
 do
-  cp $sourceDir/${Lib[i]} $targetDir/${Lib[i]}
+  cp $sourceDir/${Lib[i]} $targetDir
   echo "."
 done
+
+#同步
 sync
 sync
 sync
+
 echo "文件替换完毕！"
 echo "替换结果查看："
-for (( i = 0; i < ${#Lib[@]}; ++i ))
+for (( i = 0; i < $libLength; ++i ))
 do
   ls -l $targetDir/${Lib[i]}*
 done
@@ -63,16 +67,16 @@ do
     if [ $num == 1 ]
     then
     	echo 替换成功！删除升级文件！！！
-	echo ...
-	rm  -rf $sourceDir/*.so
+		echo ...
+		rm  -rf $sourceDir/*.so
 
-	echo 删除成功
-	echo 程序执行完毕，退出！
-	break
+		echo 删除成功
+		echo 程序执行完毕，退出！
+		break
     elif [ $num == 0 ]
     then
     	echo 升级失败，请重新运行up_lib.sh脚本！！！
-	break
+		break
     else
     	echo 输入数值错误，请重新输入:
     fi
