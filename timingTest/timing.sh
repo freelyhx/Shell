d92206d4cfb02231a56925f9e05311a05af99b31
freelyhx <787126844@qq.com>
2017-04-07 17:55:06 +0800

    优化脚本，增加全局变量，优化日志格式，使之更易查看

--- timingTest/timing.sh
+++ timingTest/timing.sh
@@ -1,28 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-# 相机启动，等待3分钟与中心校时
-echo wait 3 min
-sleep 3m
+#-----------------------------------------------------------
+# filename:timing.sh
+# version:2.0
+# date:2017/04/07
+# author:yangql
+# program:定时校时功能测试
+#-----------------------------------------------------------
+
+# 变量初始化
+COUNT=0		#计数器 初始值为0
+TIME=30		#延时时间（秒） 默认为60
+
+# 删除日志文件
+rm date.txt
 
 while true
 do
 	# Print the delimiter
 	printf "*****\n*****\n*****\n" >> date.txt	
+
+	let COUNT++
+	echo "$COUNT timing result:"
+	echo "$COUNT timing result:" >> date.txt
 	
 	# display now time
-	printf "NowTime: `date` \n" >> date.txt
+	printf "NowTime:$(date "+%Y-%m-%d %T")\n"
+	printf "NowTime:$(date "+%Y-%m-%d %T")\n" >> date.txt
 
 	# set time
-	date -s '2016-12-12 12:12:12'
-	printf "SetTime: `date` \n" >> date.txt
+	date -s '2016-12-12 12:12:12' > /dev/null
+	printf "SetTime:$(date "+%Y-%m-%d %T")\n"
+	printf "SetTime:$(date "+%Y-%m-%d %T")\n" >> date.txt
 
 	# set hwclock and display HardwareTime
 	hwclock -w
-	printf "HwTime: `date` \n" >> date.txt
+	printf "HwTime:$(hwclock)\n"
+	printf "HwTime:$(hwclock)\n" >> date.txt
 
 	# waiting for timing 设置相机定时校时间隔为1小时
-	echo wait 1 hour
-	sleep 1h
+	echo wait $TIME second
+	sleep $TIME
 done
-
-
